974cdc2cbe58a85701b87a665f0d39e9b73afc2f
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-05-14 13:55:12 +0200

    exercise done

---
+++ Script/code.js
@@ -0,0 +1,11 @@
+ let firstName = "Keenan"
+ let surName = "Timmie"
+numberVar = 5;
+const name = "Keenan"
+
+// gives error //
+// const name = "Timmie"
+
+console.log(numberVar * 5)
+
+console.log(firstName + surName);
